b036a92d4449cb40788670931139d8a6acd99be7
akineni <akinlonueniola@gmail.com>
2025-09-15 19:12:59 +0100

    style(form): center subscription form and make it responsive across screen sizes

--- src/components/SubscribeForm/SubscribeForm.jsx
+++ src/components/SubscribeForm/SubscribeForm.jsx
@@ -1,15 +1,38 @@
 import styles from './SubscribeForm.module.css';
 
 export default function SubscribeForm() {
-    return (
-        <section className={ `${styles['subscribe-form']} bg-light` }>
-            <div className="container">
-                <h3 className="mb-4">Stay Updated with Our Latest Offers</h3>
-                <form action="#" method="post">
-                    <input className="mb-3 mb-md-0" type="email" placeholder="Enter your email" required />
-                    <button type="submit" className={ styles['subscribe-btn'] }>Subscribe</button>
-                </form>
-            </div>
-        </section>
-    );
+	return (
+		<section className={`${styles['subscribe-form']} bg-light`}>
+			<div className="container">
+				<h3 className="mb-4">Stay Updated with Our Latest Offers</h3>
+				<div className="container">
+					<div className="row">
+						<div className="col-12 col-md-8 col-lg-6 mx-auto">
+							<form
+								action="#"
+								method="post"
+								className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-2"
+							>
+								<input
+									className="form-control w-100"
+									type="email"
+									placeholder="Enter your email"
+									required
+								/>
+								<button
+									type="submit"
+									className={`form-control ${styles['subscribe-btn']} w-100 w-md-auto`}
+								>
+									Subscribe
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+
+
+
+			</div>
+		</section>
+	);
 }
--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -3,15 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { supabase } from '@lib/supabaseClient';
 
 export default function AuthCallback() {
+	// https://supabase.com/docs/guides/auth/social-login/auth-google?queryGroups=framework&framework=nextjs&queryGroups=environment&environment=client#signing-users-in
 	const navigate = useNavigate();
 
 	useEffect(() => {
 		const handleSession = async () => {
 			const { data, error } = await supabase.auth.getSession();
+			
 			if (error) {
 				console.error('Auth error:', error);
 				return;
 			}
+
 			if (data?.session) {
 				navigate('/dashboard');
 			} else {
