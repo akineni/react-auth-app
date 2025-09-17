8e1d6d54cc0cbd67c383c50d78debc4547535b3e
akineni <akinlonueniola@gmail.com>
2025-09-17 23:40:11 +0100

    use github & linkedin on footer instead and format for readability.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,16 +2,31 @@ import { APP_NAME } from '@config/appConfig';
 import styles from './Footer.module.css';
 
 export default function Footer({ className }) {
-    return (
-        <footer className={ styles.footer }>
-            <div className="container">
-                <p>&copy; {new Date().getFullYear()} {APP_NAME}. All rights reserved.</p>
-                <div className={ className }>
-                    <a href="#"><i className="fab fa-facebook"></i></a>
-                    <a href="#"><i className="fab fa-instagram"></i></a>
-                    <a href="#"><i className="fab fa-twitter"></i></a>
-                </div>
-            </div>
-        </footer>
-    );
+	return (
+		<footer className={styles.footer}>
+			<div className="container">
+				<p>
+					&copy; {new Date().getFullYear()} {APP_NAME}. All rights reserved.
+				</p>
+
+				<div className={className}>
+					<a
+						href="https://github.com/akineni/react-auth-app"
+						title="View github repo"
+						target="_blank"
+					>
+						<i className="fab fa-github"></i>
+					</a>
+
+					<a
+						href="https://www.linkedin.com/in/eniola-akinlonu-b210b3164"
+						title="View linkedin post"
+						target="_blank"
+					>
+						<i className="fab fa-linkedin"></i>
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
 }
